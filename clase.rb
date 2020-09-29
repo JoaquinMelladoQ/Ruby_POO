e5c91dfb0f9bb504a3944450f2a389c027825a82
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-28 23:40:56 -0300

    Adding fourth excercise

--- clase.rb
+++ clase.rb
@@ -25,3 +25,20 @@ class Producto
         @stock = stock
     end
 end
+
+class Punto
+    attr_reader :x, :y
+    def initialize(x,y)
+        @x = x
+        @y = y
+    end
+
+    def avanzar
+        @x += 1
+    end
+end
+
+p1 = Punto.new(2,3)
+puts p1.avanzar
+
+## x avanza del 2 al 3
