717130566b897e937f5255a9f95973908a79de31
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-28 23:33:06 -0300

    Adding first excercise

---
+++ clase.rb
@@ -0,0 +1,11 @@
+class Mascota
+    attr_accessor :nombre
+    def initialize(nombre)
+        @nombre = nombre
+    end
+end
+
+primera_mascota = Mascota.new('Spike')
+# puts primera_mascota.nombre
+
+
